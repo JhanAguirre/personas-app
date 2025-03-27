bad9a58a5559159ad0be3d4aeb0f331915008783
JhanAguirre <jhanalejandroaguirre5@gmail.com>
2025-03-27 10:23:00 -0500

    Delete_Departamentos

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departamento;
+use App\Models\Pais; // Importa el modelo Pais
 use Illuminate\Http\Request;
 
 class DepartamentoController extends Controller
@@ -11,7 +13,8 @@ class DepartamentoController extends Controller
      */
     public function index()
     {
-        //
+        $departamentos = Departamento::with('pais')->get();
+        return view('departamentos.index', compact('departamentos'));
     }
 
     /**
@@ -19,7 +22,8 @@ class DepartamentoController extends Controller
      */
     public function create()
     {
-        //
+        $paises = Pais::all();
+        return view('departamentos.create', compact('paises'));
     }
 
     /**
@@ -27,7 +31,17 @@ class DepartamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'dep_nomb' => 'required|string|max:255',
+            'pais_codi' => 'required|exists:tb_pais,pais_codi', // Asegurar que el país exista
+        ]);
+
+        $departamento = new Departamento();
+        $departamento->dep_nomb = $request->input('dep_nomb');
+        $departamento->pais_codi = $request->input('pais_codi');
+        $departamento->save();
+
+        return redirect()->route('departamentos.index')->with('success', 'Departamento creado exitosamente.');
     }
 
     /**
@@ -35,7 +49,8 @@ class DepartamentoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $departamento = Departamento::with('pais')->findOrFail($id);
+        return view('departamentos.show', compact('departamento'));
     }
 
     /**
@@ -43,7 +58,9 @@ class DepartamentoController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $departamento = Departamento::findOrFail($id);
+        $paises = Pais::all();
+        return view('departamentos.edit', compact('departamento', 'paises'));
     }
 
     /**
@@ -51,7 +68,17 @@ class DepartamentoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'dep_nomb' => 'required|string|max:255',
+            'pais_codi' => 'required|exists:tb_pais,pais_codi', // Asegurar que el país exista
+        ]);
+
+        $departamento = Departamento::findOrFail($id);
+        $departamento->dep_nomb = $request->input('dep_nomb');
+        $departamento->pais_codi = $request->input('pais_codi');
+        $departamento->save();
+
+        return redirect()->route('departamentos.index')->with('success', 'Departamento actualizado exitosamente.');
     }
 
     /**
@@ -59,6 +86,8 @@ class DepartamentoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $departamento = Departamento::findOrFail($id);
+        $departamento->delete();
+        return redirect()->route('departamentos.index')->with('success', 'Departamento eliminado exitosamente.');
     }
 }
